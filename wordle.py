808e11639931032435bf75444469c41c3a8576ab
Ryan Short <shorttryan@gmail.com>
2022-10-05 15:28:32 -0400

    adds some instructions to game

---
+++ wordle.py
@@ -0,0 +1,10 @@
+# https://ozzmaker.com/add-colour-to-text-in-python/
+
+def game_instruction():
+    print("""Guess the WORDLE in 6 tries.
+    Each guess must be aa valid 5-letter word. Hit the enter key to submit.
+    After each guess, the color of the tiles will change to show how close your
+    guess was to the word.""")
+    print("-" * 75)
+    print("""Examples
+    )
